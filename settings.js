2769ac4928df7dacf8c027ae587bef3ba739af7b
fajarmelfiano <101026953+FajarMelfiano@users.noreply.github.com>
2022-06-30 10:13:42 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,33 +13,33 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 global.autobio = true 
 
 //language
 //Available in indonesia & english
 //You can request others language 
-global.language = english  //change indonesia to english if you don't understand the language used by the bot
+global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname "MT-BotzMD" //namabot kalian
+global.ownername= "FajarM" //nama kalian
+global.myweb ="https://github.com/FajarMelfiano" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/channel/UCGH2VzotNoXb0iErq52fupQ" //bebas asal jan hapus
+global.github = "https://github.com/FajarMelfiano" //bebas
+global.email = "fajarmelfiano@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["6285748839210","622150996855"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285748839210" // nomor wa kalian
+global.ownernomerr = "+6285748839210" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© MT-Md' //sticker wm ubah
+global.author = 'Di Recode oleh Fajar' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
